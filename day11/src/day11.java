e6bd01f4b61c3bd29dc8dc22869115c7b3955722
Rémi Del Medico <redelmedico@gmail.com>
2024-11-25 11:12:33 +0100

    Initial commit

---
+++ day11/src/day11.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class day11 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("./src/input.txt"))) {
+            String line;
+
+            String[] elements = null;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                elements = line.split(".");
+            }
+
+            for (int i = 0; i < elements.length; i++) {
+                System.out.println(elements[i] + "\n");
+            }
+        }
+    }
+}

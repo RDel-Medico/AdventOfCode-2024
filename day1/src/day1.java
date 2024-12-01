cfeee555e2a1e3a70ecd742e6d1f1b00a90f9f73
RDel-Medico <redelmedico@gmail.com>
2024-12-01 11:08:59 +0100

    Day 1

--- day1/src/day1.java
+++ day1/src/day1.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class day1 {
     public static void main(String[] args) throws IOException {
@@ -9,13 +10,23 @@ public class day1 {
 
             String[] elements = null;
 
+            int[] left = new int[1000];
+            HashMap<Integer, Integer> right = new HashMap<>();
+            int index = 0;
+
             while ((line = bufferedReader.readLine()) != null) {
-                elements = line.split(".");
+                elements = line.split("-");
+                left[index++] = Integer.parseInt(elements[0]);
+                right.put(Integer.parseInt(elements[1]), right.getOrDefault(Integer.parseInt(elements[1]), 0) + 1);
             }
 
-            for (int i = 0; i < elements.length; i++) {
-                System.out.println(elements[i] + "\n");
+            int res = 0;
+
+            for (int i = 0; i < left.length; i++) {
+                res += left[i] * right.getOrDefault(left[i], 0);
             }
+
+            System.out.println(res);
         }
     }
 }
